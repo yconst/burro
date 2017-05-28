7d31bad7b443500eba10a5fb0994aac93ab38f95
Yannis Chatzikonstantinou <yconst@gmail.com>
2017-05-28 15:39:34 +0300

    improve install

---
+++ install-burro.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -
+
+echo "\nBurro Installer: Installing necessary libraries\n"
+sudo apt-get update
+sudo apt-get install --assume-yes libtiff5-dev libjpeg-dev zlib1g-dev \
+  libfreetype6-dev liblcms2-dev \
+  libwebp-dev tcl8.5-dev tk8.5-dev python-tk
+sudo apt-get install python-numpy python-scipy python-pillow \
+  libhdf5-dev python-h5py
+
+echo "\nBurro Installer: Creating environment\n"
+sudo pip install virtualenv
+virtualenv --system-site-packages burro
+cd burro
+
+echo "\nBurro Installer: Preparing Burro\n"
+git clone https://github.com/yconst/burro
+cd burro
+../bin/pip install -r requirements.txt
+
+echo "\nBurro Installer: Installing submodules\n"
+echo "Burro Installer: (this can take some time..)\n"
+git submodule update --init --recursive
+../bin/pip install -r Navio2/Python/requirements.txt
+
+echo "\nBurro Installer: Creating symlinks\n"
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+ln -s $DIR/burro/burro/Navio2/Python/navio2 $DIR/burro/burro/src/navio
+
+echo "\nBurro Installer: Done. Run sudo start.sh to start Burro\n"
