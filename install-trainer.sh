5ea86affa6af1f9f2f3c380a21dd3587add11e42
Yannis Chatzikonstantinou <yconst@gmail.com>
2017-07-15 23:28:12 +0300

    add trainer installer

---
+++ install-trainer.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -
+
+echo "\nBurro Trainer Installer\n"
+
+echo "\nFor security and compatibility reasons this installer will not install any system-wide packages.\n"
+echo "\nIf you do not yet have your preferred Tensorflow version, you will have to install it yourself.\n"
+
+
+echo "\nBurro Trainer Installer: Creating environment\n"
+sudo pip install virtualenv
+virtualenv --system-site-packages burro-trainer
+cd burro-trainer
+
+echo "\nBurro Trainer Installer: Preparing Burro\n"
+git clone https://github.com/yconst/burro
+cd burro
+../bin/pip install -r requirements-trainer.txt
+
+echo "\nBurro Trainer Installer: Done.\n"
