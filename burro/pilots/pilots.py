a015173d72e6d1c4231998b9e30979669f401b40
Yannis Chatzikonstantinou <yconst@gmail.com>
2017-08-12 15:37:15 +0300

    add regression pilot

--- burro/pilots/pilots.py
+++ burro/pilots/pilots.py
@@ -2,10 +2,7 @@
 
 pilots.py
 
-Methods to create, use, save and load pilots. Pilots
-contain the highlevel logic used to determine the angle
-and throttle of a vehicle. Pilots can include one or more
-models to help direct the vehicles motion.
+Classes representing various CNN-based pilots.
 
 '''
 from __future__ import division
@@ -66,6 +63,42 @@ class KerasCategorical(BasePilot):
             yaw_binned = prediction
             throttle = 0
         yaw = methods.from_one_hot(yaw_binned)
+
+        avf = config.model.average_factor
+        yaw = avf * self.yaw + (1.0 - avf) * yaw
+        self.yaw = yaw
+        return methods.yaw_to_angle(yaw), throttle * 0.15
+
+    def pname(self):
+        return self.name or "Keras Categorical"
+
+
+class KerasRegression(BasePilot):
+    '''
+    A pilot based on a CNN with scalar output
+    '''
+    def __init__(self, model_path, **kwargs):
+        self.yaw = 0
+        self.model = keras.models.load_model(model_path)
+        super(KerasRegression, self).__init__(**kwargs)
+
+    def decide(self, img_arr):
+        if config.camera.crop_top or config.camera.crop_bottom:
+            h,w,_ = img_arr.shape
+            t = config.camera.crop_top
+            l = h - config.camera.crop_bottom
+            img_arr = img_arr[t:l,:]
+
+        img_arr = np.interp(img_arr,config.camera.output_range,
+            config.model.input_range)
+        img_arr = np.expand_dims(img_arr, axis=0)
+        prediction = self.model.predict(img_arr)
+        if len(prediction) == 2:
+            yaw = prediction[0]
+            throttle = prediction[1]
+        else:
+            yaw = prediction
+            throttle = 0
 
         avf = config.model.average_factor
         yaw = avf * self.yaw + (1.0 - avf) * yaw
