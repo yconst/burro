45b49fb5847bf660f753f1324e7690ff371f20f4
Yannis Chatzikonstantinou <yconst@gmail.com>
2017-08-13 22:09:25 +0300

    add regression trainer, move pipeline creation to helpers

--- burro/trainers/trainer.py
+++ burro/trainers/trainer.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 
 from keras.models import Model, Sequential
-from keras.layers import Input, Activation, Dense, Convolution2D, Dropout, Flatten
+from keras.layers import Input, Activation, Dense, Convolution2D, LocallyConnected2D, Dropout, Flatten
 from keras.optimizers import SGD
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import TensorBoard, ReduceLROnPlateau, ModelCheckpoint, EarlyStopping
@@ -13,27 +13,20 @@ from config import config
 import methods
 import helpers
 
-from generators.file_generators import filename_generator
-from generators.pil_generators import (image_count,
-                            image_generator, image_flip, image_resize,
-                            image_rotate, array_generator,
-                            image_voffset, image_crop)
-from generators.numpy_generators import (category_generator,
-                              brightness_shifter, batch_image_generator,
-                              center_normalize, equalize_probs, nth_select)
 
+offset = 4
 
-def train(data_dir, track, optimizer='adam', patience=10):
-    offset = 4
+gen_batch = 256
+val_batch = 32
+val_stride = 20
 
-    gen_batch = 256
-    val_batch = 32
-    val_stride = 20
+dense1 = 150
+dense2 = 50
+dense3 = 10
 
-    dense1 = 150
-    dense2 = 50
+now = time.strftime("%c")
 
-    now = time.strftime("%c")
+def train_categorical(data_dir, track, optimizer='adam', patience=15):
 
     hist = helpers.angles_histogram(data_dir)
     print hist[0]
@@ -55,36 +48,11 @@ def train(data_dir, track, optimizer='adam', patience=10):
 
     methods.create_file(model_path)
 
-    im_count = image_count(data_dir)
-    gen = filename_generator(data_dir, indefinite=True)
-    gen = nth_select(gen, mode='reject_nth', nth=10, offset=offset)
-    gen = equalize_probs(gen)
-    gen = image_generator(gen)
-    gen = image_flip(gen)
-    gen = image_voffset(gen)
-    gen = image_rotate(gen)
-    gen = image_resize(gen)
-    gen = image_crop(gen)
-    gen = array_generator(gen)
-    gen = center_normalize(gen)
-    gen = brightness_shifter(gen, min_shift=-0.28, max_shift=0.18)
-    gen = category_generator(gen)
-    gen = batch_image_generator(gen, batch_size=gen_batch)
-
-    val = filename_generator(data_dir, indefinite=True)
-    val = nth_select(val, mode='accept_nth', nth=10, offset=offset)
-    val = equalize_probs(val)
-    val = image_generator(val)
-    val = image_flip(val)
-    val = image_voffset(val)
-    val = image_rotate(val)
-    val = image_resize(val)
-    val = image_crop(val)
-    val = array_generator(val)
-    val = center_normalize(val)
-    val = brightness_shifter(val, min_shift=-0.2, max_shift=0.1)
-    val = category_generator(val)
-    val = batch_image_generator(val, batch_size=val_batch)
+    im_count = helpers.image_count(data_dir)
+    gen = helpers.categorical_pipeline(data_dir, mode='reject_nth',
+        batch_size=gen_batch, offset=offset)
+    val = helpers.categorical_pipeline(data_dir, mode='accept_nth',
+        batch_size=val_batch, offset=offset)
 
     model = Sequential()
     model.add(
@@ -109,6 +77,74 @@ def train(data_dir, track, optimizer='adam', patience=10):
     model.compile(
         optimizer=optimizer, loss={
             'angle_out': 'categorical_crossentropy'})
+
+    print model.summary()
+
+    tb = TensorBoard(log_path)
+    # reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.96,
+    #          patience=2, min_lr=0.0001)
+    model_cp = ModelCheckpoint(model_path, monitor='val_loss',
+                               save_best_only=True, mode='auto', period=1)
+    e_stop = EarlyStopping(monitor='val_loss', patience=patience)
+
+    print "Best model saved in " + model_path
+
+    hist = model.fit_generator(gen,
+                               epochs=200,
+                               steps_per_epoch=im_count / gen_batch,
+                               validation_data=val,
+                               validation_steps=im_count / (val_batch * val_stride),
+                               callbacks=[tb, model_cp, e_stop])
+
+    return np.min(hist.history['val_loss'])
+
+def train_regression(data_dir, track, optimizer='adam', patience=15):
+
+    hist = helpers.angles_histogram(data_dir)
+    print hist[0]
+    print hist[1]
+
+    input_shape=(99-config.camera.crop_top, 132, 3)
+    print "Input shape: " + str(input_shape)
+
+    models_dir = os.path.abspath(os.path.expanduser(config.training.models_dir))
+    model_path = os.path.join(models_dir,
+        'model-' + track + '-' + optimizer +
+        '-' + str(dense1) + '-' + str(dense2) +
+        '-' + now + '.h5')
+    logs_dir = os.path.abspath(os.path.expanduser(config.training.logs_dir))
+    log_path = os.path.join(logs_dir,
+    track + '/' + optimizer + '-' +
+    str(dense1) + '-' + str(dense2) +
+    '-' + now)
+
+    methods.create_file(model_path)
+
+    im_count = helpers.image_count(data_dir)
+    gen = helpers.regression_pipeline(data_dir, mode='reject_nth',
+        batch_size=gen_batch, val_every=5, offset=offset)
+    val = helpers.regression_pipeline(data_dir, mode='accept_nth',
+        batch_size=val_batch, val_every=5, offset=offset)
+
+    model = Sequential()
+    model.add(
+        Convolution2D(
+            32, (8, 8), strides=(
+                2, 2), activation='relu', input_shape=input_shape))
+    model.add(Convolution2D(32, (5, 5), strides=(2, 2), activation='relu'))
+    model.add(Convolution2D(64, (5, 5), strides=(2, 2), activation='relu'))
+    model.add(LocallyConnected2D(16, (3, 3), strides=(1, 1), activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(dense1, activation='selu'))
+    model.add( Dropout(.1) )
+    model.add(Dense(dense2, activation='selu'))
+    model.add( Dropout(.1) )
+    model.add(Dense(dense3, activation='softmax'))
+
+    model.add(Dense(1,activation='linear',name='angle_out'))
+    model.compile(
+        optimizer=optimizer, loss={
+            'angle_out': 'mean_squared_error'})
 
     print model.summary()
 
