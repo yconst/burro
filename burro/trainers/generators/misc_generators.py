4f149f06353c229e9107bcbc947706761ba02317
Yannis Chatzikonstantinou <yconst@gmail.com>
2017-08-13 22:03:50 +0300

    change generator name

--- burro/trainers/generators/misc_generators.py
+++ burro/trainers/generators/misc_generators.py
@@ -2,11 +2,18 @@
 from PIL import Image, ImageOps
 
 import methods
+from config import config
 
 
-def gen_sin(generator):
+def angle_to_sin(generator):
     '''
     Generator that converts a steering angle to sinus
     '''
     for inp, angle in generator:
         yield inp, math.sin(math.radians(angle))
+
+def angle_to_yaw(generator):
+    '''
+    '''
+    for inp, angle in generator:
+        yield inp, float(angle)/config.ackermann_car.max_steering_angle
