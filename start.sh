4d00ebb6fa4860b36d16024968d225b0ebe4c7e6
Yannis Chatzikonstantinou <yconst@gmail.com>
2017-08-19 16:36:16 +0300

    use getopts

--- start.sh
+++ start.sh
@@ -1,9 +1,22 @@
 #!/bin/sh -
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
+RECORD=false
 
-if  [[ $1 = "-r" ]]; then
-    sudo $DIR/../bin/python $DIR/burro/drive.py --record
+while getopts ":r" opt; do
+  case $opt in
+    r)
+      RECORD=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ $RECORD = true ]; then
+	sudo $DIR/../bin/python $DIR/burro/drive.py --record
 else
-    sudo $DIR/../bin/python $DIR/burro/drive.py
+	sudo $DIR/../bin/python $DIR/burro/drive.py
 fi
