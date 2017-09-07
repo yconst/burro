7d238a643e26ebcf068238120fb24157fe4d7c57
Yannis Chatzikonstantinou <yconst@gmail.com>
2017-09-07 19:29:08 +0300

    use fnmatch and search dirs recursively for images

--- burro/trainers/generators/file_generators.py
+++ burro/trainers/generators/file_generators.py
@@ -1,16 +1,15 @@
 import os
 import time
-import glob
+import fnmatch
 
 import methods
-
 
 def filename_generator(file_dir, indefinite=False, signal_end=False):
     '''
     Generator that loops (indefinitely) through
     files and telemetry data embedded in file names.
     '''
-    paths = glob.glob(os.path.join(file_dir, '*.jpg'))
+    paths = files(file_dir)
     if not paths:
         return
     while True:
@@ -21,3 +20,22 @@ def filename_generator(file_dir, indefinite=False, signal_end=False):
             break
         if signal_end:
             print "\nRestart Dataset\n"
+
+def files(file_dir, pattern='*.jpg'):
+    import ipdb; ipdb.set_trace()
+    matches = []
+    for root, dirnames, filenames in os.walk(file_dir):
+        for filename in fnmatch.filter(filenames, pattern):
+            matches.append(os.path.join(root, filename))
+        for sub_dir in dirnames:
+            matches.extend(files(os.path.join(root, sub_dir), pattern))
+    return matches
+
+def file_count(file_dir):
+    '''
+    Accepts a path and returns the image count contained
+    '''
+    paths = files(file_dir)
+    if not paths:
+        return 0
+    return len(paths)
