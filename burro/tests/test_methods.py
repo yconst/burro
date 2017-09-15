82fc64d39dc3878fb1191370e19b60828e893329
Yannis Chatzikonstantinou <yconst@gmail.com>
2017-09-16 00:21:50 +0300

    improve test suite

--- burro/tests/test_methods.py
+++ burro/tests/test_methods.py
@@ -1,5 +1,9 @@
 import unittest
-from methods import yaw_to_angle, angle_to_yaw
+import numpy as np
+
+from methods import (yaw_to_angle, angle_to_yaw, 
+					 from_index, to_index,
+					 to_one_hot, from_one_hot)
  
 class TestMethods(unittest.TestCase):
  
@@ -7,9 +11,20 @@ class TestMethods(unittest.TestCase):
         pass
  
     def test_angle_conversion(self):
-        for yaw in range(-1, 1, 0.1):
+        for yaw in np.arange(-1, 1, 0.1):
         	test_yaw = yaw_to_angle(angle_to_yaw(yaw))
         	self.assertEqual(yaw, test_yaw)
+
+    def test_binning_conversion(self):
+        for val in np.arange(-1, 1, 0.1):
+        	test_val = from_index(to_index(val))
+        	self.assertTrue(abs(val - test_val) < 0.15)
+
+    def test_one_hot_conversion(self):
+        for val in np.arange(-1, 1, 0.1):
+        	test_oh = to_one_hot(val)
+        	test_val = from_one_hot(test_oh)
+        	self.assertTrue(abs(val - test_val) < 0.2)
 
  
 if __name__ == '__main__':
