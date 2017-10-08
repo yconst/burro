e2b33f09bd47f882f36d45c7496bc1e5d529afea
Yannis Chatzikonstantinou <yconst@gmail.com>
2017-10-08 16:32:53 +0300

    add file shuffling

--- burro/trainers/generators/file_generators.py
+++ burro/trainers/generators/file_generators.py
@@ -1,15 +1,17 @@
 import os
 import time
 import fnmatch
+from random import shuffle
 
 import methods
 
-def filename_generator(file_dir, indefinite=False, signal_end=False):
+def filename_generator(file_dir, indefinite=False,
+                       shuffle_matches = True, signal_end=False):
     '''
     Generator that loops (indefinitely) through
     files and telemetry data embedded in file names.
     '''
-    paths = files(file_dir)
+    paths = files(file_dir, shuffle_matches = shuffle_matches)
     if not paths:
         return
     while True:
@@ -21,14 +23,15 @@ def filename_generator(file_dir, indefinite=False, signal_end=False):
         if signal_end:
             print "\nRestart Dataset\n"
 
-def files(file_dir, pattern='*.jpg'):
-    import ipdb; ipdb.set_trace()
+def files(file_dir, pattern='*.jpg', shuffle_matches = False):
     matches = []
     for root, dirnames, filenames in os.walk(file_dir):
         for filename in fnmatch.filter(filenames, pattern):
             matches.append(os.path.join(root, filename))
         for sub_dir in dirnames:
             matches.extend(files(os.path.join(root, sub_dir), pattern))
+    if shuffle_matches:
+        shuffle(matches)
     return matches
 
 def file_count(file_dir):
