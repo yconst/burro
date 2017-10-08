cb89e98cd0a08d1250c54e4fcb83a3a2dd83082e
Yannis Chatzikonstantinou <yconst@gmail.com>
2017-10-08 16:33:44 +0300

    add option for model type, capitalize options

--- burro/view.py
+++ burro/view.py
@@ -6,10 +6,11 @@ view.py
 View resulting images of a generator pipeline
 
 Usage:
-    view.py --data-dir <dir>
+    view.py --data-dir <DIR>, [--mode <MODE>]
 
 Options:
-  --data-dir <dir>      data directory
+  --data-dir <DIR>      data directory
+  --mode <MODE>         either regression or categorical [default: categorical]
 """
 
 import sys
@@ -20,14 +21,18 @@ from docopt import docopt
 import methods
 import config
 
-from trainers.img_pipelines import regression_pipeline
+from trainers.img_pipelines import categorical_pipeline, regression_pipeline
 from trainers.generators.pil_generators import show_image
 
 
 def main():
     arguments = docopt(__doc__)
     data_dir = arguments['--data-dir']
-    pipeline = regression_pipeline(data_dir, val_every=0, batch_size=1)
+    mode = arguments['--mode']
+    if mode == 'regression':
+        pipeline = regression_pipeline(data_dir, val_every=0, batch_size=1)
+    elif mode == 'categorical':
+        pipeline = categorical_pipeline(data_dir, val_every=0, batch_size=1)
     show_image(pipeline)
 
 
